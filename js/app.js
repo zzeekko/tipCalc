fa255c277fd139bea80f4fb35ea2ee4f8ca38949
zzeekko <ezfirststep@gmail.com>
2024-11-20 11:56:14 -0600

    need to fix add to cart button

--- js/app.js
+++ js/app.js
@@ -223,7 +223,7 @@ menuDivs.forEach(div => {
 
 for (let i = 0; i < menuTypes.length; i++) {
     menuDivs[i].children[0].innerText = menuTypes[i]
-    menuDivs[i].children[1].setAttribute('id', `${menuTypes[1]}Row`)
+    menuDivs[i].children[1].setAttribute('id', `${menuTypes[i]}Row`)
     console.log(menuDivs[i])
 }
 // grab the appRow
@@ -240,7 +240,7 @@ menuItems.forEach(item => {
     column.classList.add('col-sm-3')
 
     const card = document.createElement('div')
-    card.classList.add('card')
+    card.classList.add('card', 'h-100')
 
     card.innerHTML = `
     <img src="images/${item.imgUrl} alt="${item.desc}" class="img-fluid menu-image card-image-top" />
@@ -248,5 +248,165 @@ menuItems.forEach(item => {
         <h4 class="card-title text-capitalize item-item">${item.item}</h4>
         <p class="card-text text-uppercase item-desc">${item.desc}</p>
     </div>
+    <footer class="card-footer">
+        <p class="card-text item-price">$${item.price}</p>
+        <div class="buttons-div d-flex justify-content-around">
+            <button 
+                class="btn btn-danger cart-btn text-capitalize"
+                id="Btn${item.id}"
+                data-id="${item.id}"
+                data-price="${item.price}"
+                data-qty="${item.qty}"
+                data-item="${item.item}"
+            >add to card</button>
+            <div class="qty-div">
+                <button
+                    class="btn btn-primary btn-subtract"
+                    id="btnSubtract${item.id}"
+                    data-id="${item.id}"
+                    data-qty="${item.qty}"
+                > - </button>
+                <span class="quantity" id="quantity${item.id}">${item.qty}</span>
+                <button
+                    class="btn btn-primary btn-add"
+                    id="btnAdd${item.id}"
+                    data-id="${item.id}"
+                    data-qty="${item.qty}"
+                > + </button>
+            </div>
+        </div>
+    </footer>
     `
+
+    column.appendChild(card)
+
+    // switch (menuItems.type)
+    switch (item.type) {
+        case 'appetizers':
+            appRow.appendChild(column)
+            break
+        case 'entrees':
+            entreesRow.appendChild(column)
+            break
+        case 'drinks':
+            drinksRow.appendChild(column)
+            break
+        case 'desserts':
+            dessertsRow.appendChild(column)
+            break
+        case 'sides':
+            sidesRow.appendChild(column)
+            break
+        default:
+            console.log('Error: menu type not listed')
+            break
+    }
+})
+
+// cart buttons
+const cartButtons = document.querySelectorAll('.cart-btn')
+
+// console.log(cartButtons);
+cartButtons.forEach(button => {
+
+    const price = parseFloat(button.getAttribute('data-price'))
+    const item = getAttribute('data-item')
+    const id = parseFloat(button.getAttribute('data-id'))
+
+    button.addEventListener('click', ()=> {
+        let qty
+        for (let i = 0; i < menuItems.length; i++) {
+            menuItems[i].id === id ? qty = menuItems[i].qty : null
+        }
+        addItems(price, qty, item, id)
+    })
+})
+
+// addItems()
+let receiptArray = []
+
+const addItems =(price, qty, item, id)=> {
+
+    let itemObj = {
+        id: id,
+        item: item,
+        qty: qty,
+        price: price,
+        itemTotal: qty * price
+    }
+
+    receiptArray = [...receiptArray, itemObj]
+
+    //makeReceipt
+    makeReceipt(itemObj, receipt)
+
+    subtotal+= itemObj.itemTotal
+    cartSubtotal.innerText = subtotal.toFixed(2)
+}
+
+// makeReceipt()
+const makeReceipt =(obj, el)=> {
+    const tableRow = document.createElement('tr')
+    tableRow.classList.add('receipt-item')
+
+    const receiptChoice = document.createElement('td')
+    receiptChoice.classList.add('receipt-choice', 'text-center')
+    receiptChoice.innerText = obj.item
+
+    const receiptQty = document.createElement('td')
+    receiptQty.classList.add('receipt-qty', 'text-center')
+    receiptQty.setAttribute('id', `qty${obj.id}`)
+    receiptQty.innerText = obj.qty
+    
+    const receiptPrice = document.createElement('td')
+    receiptPrice.classList.add('receipt-price', 'text-center')
+    receiptPrice.innerText = obj.price
+
+    const itemSubtotal = document.createElement('td')
+    itemSubtotal.classList.add('item-subtotal', 'text-center')
+    itemSubtotal.setAttribute('id', `subTotal${obj.id}`)
+    itemSubtotal.innerText = obj.itemTotal
+
+    tableRow.appendChild(receiptChoice)
+    tableRow.appendChild(receiptQty)
+    tableRow.appendChild(receiptPrice)
+    tableRow.appendChild(itemSubtotal)
+
+    el.appendChild(tableRow)
+
+}
+
+const btnSubtract = document.querySelectorAll('btn-subtract')
+const btnAdd = document.querySelectorAll('.btn-add')
+
+btnSubtract.forEach(button => {
+    button.addEventListener('click', ()=> {
+        const btnId = parseFloat(button.getAttribute('data-id'))
+        const spanQty = document.getElementById(`quantity${btnId}`)
+
+        for (let i = 0; i < menuItems.length; i++) {
+            if(menuItems[i].id === btnId && menuItems[i].qty > 0) {
+                menuItems[i].qty-= 1
+                spanQty.innerText = menuItems[i].qty
+            }
+        }
+    })
+})
+
+btnAdd.forEach(button => {
+    button.addEventListener('click', ()=> {
+        const btnId = parseFloat(button.getAttribute('data-id'))
+        const spanQty = document.getElementById(`quantity${btnId}`)
+
+        for (let i = 0; i < menuItems.length; i++) {
+            if (menuItems[i].id == btnId
+                && menuItems[i].qty < 20
+                && cartButtons[i].dataset.id == btnId
+            ) {
+                menuItems[i].qty+=1
+                cartButtons[i].setAttribute('data-qty', menuItems[i].qty)
+                spanQty.innerText = menuItems[i].qty
+            }
+        }
+    })
 })
